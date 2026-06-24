84dea37ef61fbf3e82283e0e5d33d02d951f76c8
EmjayBot <71861583+EmjayBot@users.noreply.github.com>
2026-06-23 23:34:36 -0500

    Remove Astro, fix worker filename, deploy to status.wayfield.dev

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://wayfield.dev',
-  base: '/',
-  integrations: [tailwind()],
-  output: 'static'
-});
